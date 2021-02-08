7999d6bc7936566802eeba03a6197d47443ad58e
Filipe Pina <fopina@gmail.com>
2021-02-08 01:02:16 +0000

    simple integration test

--- test.sh
+++ test.sh
@@ -4,16 +4,43 @@ set -e
 
 cd $(dirname $0)
 
+function cleanup() {
+        echo cleaning up
+        docker rm -f test-fluent-plugin || true
+}
+
 docker run --rm \
            -v $(pwd):/myplugin fopina/fluent-bit-plugin-dev:v1.6.10-1 \
            sh -c "cmake -DFLB_SOURCE=/usr/src/fluentbit/fluent-bit-1.6.10/ \
                  -DPLUGIN_NAME=out_influxdb_v2 ../ && make"
 
+cleanup
+docker run --rm -p 8086 --name test-fluent-plugin -d quay.io/influxdb/influxdb:v2.0.3
+trap "cleanup" EXIT
+while [ 1 ]; do
+        sleep 1
+        echo "pinging influxd..."
+        if docker exec test-fluent-plugin influx ping &> /dev/null; then
+                echo "got response from influxd, proceeding"
+                break
+        fi
+done
+
+docker exec test-fluent-plugin influx setup \
+                                      --force \
+                                      --username admin \
+                                      --password adminadmin \
+                                      --org test \
+                                      --bucket fluentbit \
+                                      --token xxx
+
 docker run --rm \
-           -v $(pwd)/build:/myplugin fluent/fluent-bit:1.6.10 \
+           -v $(pwd)/build:/myplugin \
+           --link test-fluent-plugin:influxdb \
+           fluent/fluent-bit:1.6.10 \
            /fluent-bit/bin/fluent-bit -v \
-           -q -f 1 \
+           -f 1 \
            -e /myplugin/flb-out_influxdb_v2.so \
            -i mem \
-           -o stdout -m '*' \
+           -o influxdb_v2://influxdb/ -p org=test -p http_token=xxx -m '*' \
            -o exit -m '*'
